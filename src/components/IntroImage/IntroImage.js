496cf7115b8b426249f00dfcb6183268806b1a67
iancleary <hello@iancleary.me>
2022-02-15 13:42:06 -0700

    Add IntroImage for DNPNA Logo

---
+++ src/components/IntroImage/IntroImage.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Image from "next/image";
+import styled from 'styled-components';
+
+
+class IntroImage extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Image
+          src="/dnpna-logo.jpg"
+          quality={100}
+          layout="fill"
+          alt="Avatar"
+        />
+      </Wrapper>
+   );
+  }
+}
+
+// 2257 × 1404
+
+const Wrapper = styled.div`
+  border-radius: 25px;
+  width: 600px;
+  height: 373px;
+  @media (max-width: 400px) {
+    width: 300px;
+    height: 187px;
+  }
+  position: relative;
+  overflow: hidden;
+  margin:50px auto; /* centered */
+`;
+
+export default IntroImage;
